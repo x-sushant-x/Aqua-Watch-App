250af70aea2a3b4fe2b031b70f8414fe69942ab6
Hemish <hemish04082005@gmail.com>
2023-09-03 17:30:54 +0530

    add routes and add pubspec.lock to ignore

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aqua Watch',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,6 +25,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //routes: const {
+      //  "/": SplashPage(),
+      //},
     );
   }
 }
